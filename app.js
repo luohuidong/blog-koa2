6a202a246a74eebf193e6b6e5c3592138657cbf0
luohuidong <luohuidong01@126.com>
2019-03-29 18:33:42 +0800

    koa2 脚手架初始化

---
+++ app.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const fs = require('fs')
+
+require('dotenv').config()
+
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+const morgan = require('koa-morgan')
+
+const index = require('./routes/index')
+const login = require('./routes/login')
+const blog = require('./routes/blog')
+
+
+const REDIS_CONF = require('./config/redis')
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// 配置日志
+const ENV = process.env.NODE_ENV
+if (ENV !== 'production') {
+  // 开发环境
+  app.use(morgan('dev')) // 日志
+} else {
+  // 线上环境
+  const logFileName = path.join(__dirname, './logs/access.log')
+  const writeStream = fs.createWriteStream(logFileName, {
+    flags: 'a'
+  })
+  app.use(morgan('combined', {
+    stream: writeStream
+  }))
+}
+
+// session 配置
+app.keys = ['asdfafda']
+app.use(session({
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  })
+}))
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(login.routes(), login.allowedMethods())
+app.use(blog.routes(), blog.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
